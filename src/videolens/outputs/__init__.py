c7cba2b6c6b36b2ef3a8caee128ba56fdb1e0f08
Ianwusb <3216243013@qq.com>
2026-07-19 11:16:41 +0800

    test: push single small file to verify MCP GitHub connection

--- src/videolens/outputs/__init__.py
+++ src/videolens/outputs/__init__.py
@@ -1,5 +1,4 @@
 from videolens.outputs.write_json import write_json
 from videolens.outputs.write_markdown import render_markdown, write_markdown
-from videolens.outputs.write_pdf import render_pdf, write_pdf
 
-__all__ = ["write_json", "write_markdown", "render_markdown", "write_pdf", "render_pdf"]
+__all__ = ["write_json", "write_markdown", "render_markdown"]
